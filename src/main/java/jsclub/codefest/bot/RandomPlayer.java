46905fc72ecf6ca724ddd69fffc76f5e8e8cdedb
D.A.N_3002 <dinhanh300229@gmail.com>
2022-08-08 20:43:34 +0700

    Update bot example

--- src/main/java/jsclub/codefest/bot/RandomPlayer.java
+++ src/main/java/jsclub/codefest/bot/RandomPlayer.java
@@ -4,13 +4,15 @@ package jsclub.codefest.bot;
 import io.socket.emitter.Emitter;
 import jsclub.codefest.bot.constant.GameConfig;
 import jsclub.codefest.sdk.model.Hero;
-// import jsclub.codefest.sdk.socket.data.GameInfo;
-// import jsclub.codefest.sdk.util.GameUtil;
+import jsclub.codefest.sdk.socket.data.GameInfo;
+import jsclub.codefest.sdk.util.GameUtil;
 
 import java.util.Random;
 
 public class RandomPlayer {
     final static String SERVER_URL = "https://codefest.jsclub.me/";
+    final static String PLAYER_ID = "player2-xxx";
+    final static String GAME_ID = "da2d6f34-47a4-4470-9503-b134038e2247";
 
     public static String getRandomPath(int length) {
         Random rand = new Random();
@@ -25,12 +27,12 @@ public class RandomPlayer {
     }
 
     public static void main(String[] args) {
-        Hero player2 = new Hero("player2-xxx", GameConfig.GAME_ID);
+        Hero randomPlayer = new Hero(PLAYER_ID, GAME_ID);
         Emitter.Listener onTickTackListener = objects -> {
-            // GameInfo gameInfo = GameUtil.getGameInfo(objects);
-            player2.move(getRandomPath(10));
+            GameInfo gameInfo = GameUtil.getGameInfo(objects);
+            randomPlayer.move(getRandomPath(10));
         };
-        player2.setOnTickTackListener(onTickTackListener);
-        player2.connectToServer(SERVER_URL);
+        randomPlayer.setOnTickTackListener(onTickTackListener);
+        randomPlayer.connectToServer(SERVER_URL);
     }
 }
